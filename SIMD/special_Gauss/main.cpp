def59ad9b707511e15600f4f220d94e581d61d2c
NKU-LSL <3031907544@qq.com>
2024-05-18 23:27:57 +0800

    update

--- SIMD/special_Gauss/main.cpp
+++ SIMD/special_Gauss/main.cpp
@@ -3,25 +3,37 @@
 #include<sstream>
 #include<cmath>
 #include<bitset>
+#include<Windows.h>
 #include <nmmintrin.h>
 #include <immintrin.h>
-#include <windows.h>
-#include <pmmintrin.h>
-#include <xmmintrin.h>
-#include <emmintrin.h>
-#include <smmintrin.h>
-#include <tmmintrin.h>
 using namespace std;
 const int ColNum = 2362;
-const int R_RowNum = ColNum;
-const int R_ColNum = ColNum/ 32 + (ColNum%32>0);
+const int R_RowNum = 2362;
+const int R_ColNum = (ColNum+31)/ 32;
 const int E_RowNum = 453;
-const int E_ColNum = ColNum/ 32 + (ColNum%32>0);
+const int E_ColNum = (ColNum+31)/ 32;
 unsigned int E[E_RowNum][E_ColNum]= {0},R[R_RowNum][R_ColNum]= {0};
 int First[E_RowNum]= {0};
+void Init()
+{
+    for (int i = 0; i < R_RowNum; i++)
+    {
+        for (int j = 0; j < R_ColNum; j++)
+        {
+            R[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < E_RowNum; i++)
+    {
+        for (int j = 0; j < E_ColNum; j++)
+        {
+            E[i][j] = 0;
+        }
+    }
+}
 void Init_E()
 {
-    ifstream file("C:\\Users\\86178\\Desktop\\Parallel\\SIMD\\special_Gauss\\1_A.txt");
+    ifstream file("C:\\Users\\86178\\Desktop\\Parallel\\Para_Code\\SIMD\\special_Gauss\\data\\2362_E.txt");
     string line;
     int index = 0;
     while (getline(file, line))
@@ -31,12 +43,11 @@ void Init_E()
         bool isFirst = false;
         while (iss >> number)
         {
-            if (isFirst == false)   //判断是不是首项
+            if (isFirst == false)
             {
                 First[index] = number;
                 isFirst = true;
             }
-            //将数字存入矩阵中
             int offset = number % 32;
             int post = number / 32;
             int temp = 1 << offset;
@@ -48,7 +59,7 @@ void Init_E()
 }
 void Init_R()
 {
-    ifstream file("C:\\Users\\86178\\Desktop\\Parallel\\SIMD\\special_Gauss\\1_B.txt");
+    ifstream file("C:\\Users\\86178\\Desktop\\Parallel\\Para_Code\\SIMD\\special_Gauss\\data\\2362_R.txt");
     string line;
     int index = 0;
     while (getline(file, line))
@@ -74,183 +85,198 @@ void Init_R()
 }
 void normal()
 {
-    for (int i = 0; i < Ma1_RowNum; i++)
+    for (int i = 0; i < E_RowNum; i++)
     {
         while (First[i] != -1)
         {
-            for (int i = 0; i < Ma2_ColNum; i++)
+            bool isnull=true;
+            for (int h = 0; h < R_ColNum; h++)
             {
-                if (Ma2[First[i]][i] != 0)
+                if (R[First[i]][h] != 0)
                 {
-                    for (int j = 0; j < Ma2_ColNum; j++)
+                    isnull=false;
+                    for (int j = 0; j < R_ColNum; j++)
                     {
-                        Ma2[First[i]][j] = E[i][j];
+                        E[i][j] = E[i][j] ^ R[First[i]][j];
                     }
-
-                }
-                else
-                {
-                    for (int j = 0; j < Ma1_ColNum; j++)
-                    {
-                        Ma1[i][j] = Ma1[i][j] ^ Ma2[First[i]][j];
-                    }
-                    int j;
-                    for (j = 0; j < Ma1_ColNum; j++)
-                    {
-                        if (Ma1[i][j] != 0)
-                        {
+                    int k;
+                    for (k = 0; k < E_ColNum; k++)
+                        if (E[i][k] != 0)
                             break;
-                        }
-                    }
-                    if (j == Ma1_ColNum)
+                    if (k == E_ColNum)
                     {
                         First[i] = -1;
-                        return;
+                        break;
                     }
-                    unsigned int temp = Ma2[i][j];
-                    int k = 0;
+                    unsigned int temp = E[i][k];
+                    int j = 0;
                     while (temp != 0)
                     {
                         temp = temp >> 1;
-                        k++;
+                        j++;
                     }
-                    First[i] = Ma1_ColNum * 32 - (j + 1) * 32 + k - 1;
+                    First[i] = E_ColNum * 32 - (k + 1) * 32 + j - 1;
                 }
+            }
+            if(isnull)
+            {
+                for (int j = 0; j < R_ColNum; j++)
+                    R[First[i]][j] = E[i][j];
+                break;
             }
         }
     }
 }
 void sse()
 {
-    for (int i = 0; i < Ma1_RowNum; i++)
+    for (int i = 0; i < E_RowNum; i++)
     {
         while (First[i] != -1)
         {
-            for (int i = 0; i < Ma2_ColNum; i++)
+            bool isnull=true;
+            for (int h = 0; h < R_ColNum; h++)
             {
-                if (Ma2[First[i]][i] != 0)
+                if (R[First[i]][h] != 0)
                 {
-                    for (int j = 0; j < Ma2_ColNum; j++)
+                    isnull=false;
+                    int num = 0;
+                    for (int h = 0; h +4<= E_ColNum; h+=4,num=h)
                     {
-                        Ma2[First[i]][j] = E[i][j];
+                        __m128i vec_E = _mm_loadu_si128((__m128i*)(E[i] + h));
+                        __m128i vec_R = _mm_loadu_si128((__m128i*)(R[First[i]] + h));
+                        vec_E = _mm_xor_si128(vec_E, vec_R);
+                        _mm_storeu_si128((__m128i*)(E[i]+h), vec_E);
+                    }
+                    for (int h = num; h < E_ColNum; h++)
+                    {
+                        E[i][h] = E[i][h] ^ R[First[i]][h];
                     }
 
-                }
-                else
-                {
-                    int num = 0;
-                    for (int i = 0; i < E_ColNum - 3; i+=4,num=i)
-                    {
-                        __m128i t1 = _mm_loadu_si128((__m128i*)(Ma1[i] + k));
-                        __m128i t2 = _mm_loadu_si128((__m128i*)(Ma2[First] + k));
-                        t1 = _mm_xor_si128(t1, t2);
-                        _mm_storeu_si128((__m128i*)(E[i]+k), t1);
-                    }
-                    for (int j = 0; j < Ma1_ColNum; j++)
-                    {
-                        Ma1[i][j] = Ma1[i][j] ^ Ma2[First[i]][j];
-                    }
-                    int j;
-                    for (j = 0; j < Ma1_ColNum; j++)
-                    {
-                        if (Ma1[i][j] != 0)
-                        {
+                    int k;
+                    for (k = 0; k < E_ColNum; k++)
+                        if (E[i][k] != 0)
                             break;
-                        }
-                    }
-                    if (j == Ma1_ColNum)
+                    if (k == E_ColNum)
                     {
                         First[i] = -1;
-                        return;
+                        break;
                     }
-                    unsigned int temp = Ma2[i][j];
-                    int k = 0;
+                    unsigned int temp = E[i][k];
+                    int j = 0;
                     while (temp != 0)
                     {
                         temp = temp >> 1;
-                        k++;
+                        j++;
                     }
-                    First[i] = Ma1_ColNum * 32 - (j + 1) * 32 + k - 1;
+                    First[i] = E_ColNum * 32 - (k + 1) * 32 + j - 1;
                 }
+            }
+            if(isnull)
+            {
+                for (int j = 0; j < R_ColNum; j++)
+                    R[First[i]][j] = E[i][j];
+                break;
             }
         }
     }
 }
 void avx()
 {
-    for (int i = 0; i < Ma1_RowNum; i++)
+    for (int i = 0; i < E_RowNum; i++)
     {
         while (First[i] != -1)
         {
-            for (int i = 0; i < Ma2_ColNum; i++)
+            bool isnull=true;
+            for (int h = 0; h < R_ColNum; h++)
             {
-                if (Ma2[First[i]][i] != 0)
+                if (R[First[i]][h] != 0)
                 {
-                    for (int j = 0; j < Ma2_ColNum; j++)
+                    isnull=false;
+                    int num = 0;
+                    for (int h = 0; h +8<= E_ColNum; h+=8,num=h)
                     {
-                        Ma2[First[i]][j] = E[i][j];
+                        __m256i vec_E = _mm256_loadu_si256((__m256i*)(E[i] + h));
+                        __m256i vec_R = _mm256_loadu_si256((__m256i*)(R[First[i]] + h));
+                        vec_E = _mm256_xor_si256(vec_E, vec_R);
+                        _mm256_storeu_si256((__m256i*)(E[i] + h), vec_E);
+                    }
+                    for (int h = num; h < E_ColNum; h++)
+                    {
+                        E[i][h] = E[i][h] ^ R[First[i]][h];
                     }
 
-                }
-                else
-                {
-                    int num = 0;
-                    for (int i = 0; i +4<= E_ColNum; i+=4,num=i)
-                    {
-                        __m256i t1 = _mm256_loadu_si256((__mm256i*)(Ma1[i] + k));
-                        __m256i t2 = _mm256_loadu_si256((__m256i*)(Ma2[First] + k));
-                        t1 = _mm256_xor_si256(t1, t2);
-                        _mm256_storeu_si256((__m128i*)(E[i]+k), t1);
-                    }
-                    for (int j = 0; j < Ma1_ColNum; j++)
-                    {
-                        Ma1[i][j] = Ma1[i][j] ^ Ma2[First[i]][j];
-                    }
-                    int j;
-                    for (j = 0; j < Ma1_ColNum; j++)
-                    {
-                        if (Ma1[i][j] != 0)
-                        {
+                    int k;
+                    for (k = 0; k < E_ColNum; k++)
+                        if (E[i][k] != 0)
                             break;
-                        }
-                    }
-                    if (j == Ma1_ColNum)
+                    if (k == E_ColNum)
                     {
                         First[i] = -1;
-                        return;
+                        break;
                     }
-                    unsigned int temp = Ma2[i][j];
-                    int k = 0;
+                    unsigned int temp = E[i][k];
+                    int j = 0;
                     while (temp != 0)
                     {
                         temp = temp >> 1;
-                        k++;
+                        j++;
                     }
-                    First[i] = Ma1_ColNum * 32 - (j + 1) * 32 + k - 1;
+                    First[i] = E_ColNum * 32 - (k + 1) * 32 + j - 1;
                 }
+            }
+            if(isnull)
+            {
+                for (int j = 0; j < R_ColNum; j++)
+                    R[First[i]][j] = E[i][j];
+                break;
             }
         }
     }
 }
+
 int main()
 {
     int epoch = 10;
     long long head, tail, freq;
     long double timer=0;
-    QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
-    Init_E();
-    Init_R();
-    cout<<E_ColNum<<" "<<E_RowNum<<" "<<R_ColNum<<" "<<R_RowNum<<endl;
-    //串行算法计时
     for (int i = 0; i < epoch; i++)
     {
+        Init();
         Init_E();
         Init_R();
+        QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
         QueryPerformanceCounter((LARGE_INTEGER*)&head);
-        serial();
+        normal();
         QueryPerformanceCounter((LARGE_INTEGER*)&tail);
         timer += (tail - head) * 1000.0 / freq;
     }
-    cout  << timer / epoch << "ms" << endl;
+    cout<<endl  << timer / epoch << "ms" << endl;
+    timer=0;
+    for (int i = 0; i < epoch; i++)
+    {
+        Init();
+        Init_E();
+        Init_R();
+        QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+        QueryPerformanceCounter((LARGE_INTEGER*)&head);
+        sse();
+        QueryPerformanceCounter((LARGE_INTEGER*)&tail);
+        timer += (tail - head) * 1000.0 / freq;
+    }
+    cout<<endl  << timer / epoch << "ms" << endl;
+    timer=0;
+    for (int i = 0; i < epoch; i++)
+    {
+        Init();
+        Init_E();
+        Init_R();
+        QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+        QueryPerformanceCounter((LARGE_INTEGER*)&head);
+        avx();
+        QueryPerformanceCounter((LARGE_INTEGER*)&tail);
+        timer += (tail - head) * 1000.0 / freq;
+    }
+    cout<<endl  << timer / epoch << "ms" << endl;
     return 0;
 }
+
